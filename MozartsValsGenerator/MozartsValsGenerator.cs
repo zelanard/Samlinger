ac9a9d6484d678aa3f5f6f88b4f5ce473df311fb
Jonas Offersen <72074478+zelanard@users.noreply.github.com>
2023-08-15 21:49:54 +0200

    Add project files.

---
+++ MozartsValsGenerator/MozartsValsGenerator.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using MozartsValsGenerator.Properties;
+
+namespace MozartsValsGenerator
+{
+    internal class MozartsValsGenerator
+    {
+        #region view
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sound"></param>
+        private static void PlayValtz(SoundPlayer[] sound)
+        {
+            for (int i = 0; i < sound.Length; i++)
+            {
+                sound[i].PlaySync();
+                Console.Write(i + " ");
+            }
+        }
+
+        string[,] str = new string[11, 16];
+        #endregion
+
+        #region model
+        static int[,] partsOfMinuette = new int[11, 16]
+        {
+                { 96, 22, 141, 41, 105, 122, 11, 30, 70, 121, 26, 9, 112, 49, 109, 14 },
+                { 32, 6, 128, 63, 146, 46, 134, 81, 117, 39, 126, 56, 174, 18, 116, 83 },
+                { 69, 95, 158, 13, 153, 55, 110, 24, 66, 139, 15, 132, 73, 58, 145, 79 },
+                { 40, 17, 113, 85, 161, 2, 159, 100, 90, 176, 7, 34, 67, 160, 52, 170 },
+                { 148, 74, 163, 45, 80, 97, 36, 107, 25, 143, 64, 125, 76, 136, 1, 93 },
+                { 104, 157, 27, 167, 154, 68, 118, 91, 138, 71, 150, 29, 101, 162, 23, 151 },
+                { 152, 60, 171, 53, 99, 133, 21, 127, 16, 155, 57, 175, 43, 168, 89, 172 },
+                { 119, 84, 114, 50, 140, 86, 169, 94, 120, 88, 48, 166, 51, 115, 72, 111 },
+                { 98, 142, 42, 156, 75, 129, 62, 123, 65, 77, 19, 82, 137, 38, 149, 8 },
+                { 3, 87, 165, 61, 135, 47, 147, 33, 102, 4, 31, 164, 144, 59, 173, 78 },
+                { 54, 130, 10, 103, 28, 37, 106, 5, 35, 20, 108, 92, 12, 124, 44, 131 }
+        };
+        static int[,] partsOfTrio = new int[6, 16]
+        {
+                { 72, 6, 59, 25, 81, 41, 89, 13, 36, 5, 46, 79, 30, 95, 19, 66 },
+                { 56, 82, 42, 74, 14, 7, 26, 71, 76, 20, 64, 84, 8, 35, 47, 88 },
+                { 75, 39, 54, 1, 65, 43, 15, 80, 9, 34, 93, 48, 69, 58, 90, 21 },
+                { 40, 73, 16, 68, 29, 55, 2, 61, 22, 67, 49, 77, 57, 87, 33, 10 },
+                { 83, 3, 28, 53, 37, 17, 44, 70, 63, 85, 32, 96, 12, 23, 50, 91 },
+                { 18, 45, 62, 38, 4, 27, 52, 94, 11, 92, 24, 86, 51, 60, 78, 31 }
+        };
+        private static SoundPlayer[] GetSounds(bool minuette)
+        {
+            Random rnd = new Random();
+
+            SoundPlayer[] min = new SoundPlayer[16];
+            for (int i = 0; i < min.Length; i++)
+            {
+                SoundPlayer player = new SoundPlayer();
+                if (minuette)
+                {
+                    player.SoundLocation = $"D:\\Skole\\Source\\Opgaver\\Samlinger\\MozartsValsGenerator\\Resources\\M{partsOfMinuette[rnd.Next(1, 7) + rnd.Next(0, 5), i]}.wav";
+                }
+                else
+                {
+                    player.SoundLocation = $"D:\\Skole\\Source\\Opgaver\\Samlinger\\MozartsValsGenerator\\Resources\\T{partsOfTrio[rnd.Next(0, 6), i]}.wav";
+                }
+                player.Load();
+
+                min[i] = player;
+            }
+            return min;
+        }
+
+        #endregion
+
+        #region controller
+        private static void ExecuteController()
+        {
+            SoundPlayer[] minuette = GetSounds(true);
+            SoundPlayer[] trio = GetSounds(false);
+
+            //Alternativt kunne man have lavet et array af Resources.Filnavn
+            PlayValtz(minuette);
+            Console.WriteLine();
+            PlayValtz(trio);
+        }
+
+        #endregion
+
+        static void Main(string[] args)
+        {
+            ExecuteController();
+        }
+    }
+}
---
+++ VinosBlancos/VinosBlancos.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinosBlancos
+{
+    internal class VinosBlancos
+    {
+        public const int MAX = 175388;
+        public const byte MAX_STAR = 100;
+        public const int YEAR = 0;
+        public const int SALE = 1;
+        static void Main(string[] args)
+        {
+            Execute();
+        }
+
+        #region Model
+        static int[] GetStars(bool sort, int type, int year = 0, int modification = 0)
+        {
+            int[,] wineSale = new int[11, 2] {
+                {2009,175134},
+                {2010,175388},
+                {2011,172818},
+                {2012,142709},
+                {2013,151437},
+                {2014,152620},
+                {2015,150979},
+                {2016,152210},
+                {2017,149450},
+                {2018,154398},
+                {2019,150160}
+            };
+            int[] years = new int[11];
+            int[] sales = new int[11];
+
+            //Correct Error for 2014
+            for (int i = 0; i < wineSale.GetLength(0); i++)
+            {
+                if (wineSale[i, 0] == year)
+                {
+                    wineSale[i, 0] += modification;
+                }
+            }
+
+            for (int i = 0; i < wineSale.GetLength(0); i++)
+            {
+                years[i] = wineSale[i, 0];
+                sales[i] = wineSale[i, 1];
+            }
+
+            if (sort)
+            {
+                Array.Sort(sales);
+                Array.Reverse(sales);
+                for (int i = 0; i < years.Length; i++)
+                {
+                    for (int j = 0; j < years.Length; j++)
+                    {
+                        if (sales[j] == wineSale[i, 1])
+                        {
+                            years[j] = wineSale[i, 0];
+                        }
+                    }
+                }
+            }
+
+            switch (type)
+            {
+                case YEAR:
+                    {
+                        return years;
+                    }
+                case SALE:
+                    {
+                        return sales;
+                    }
+                default:
+                    return null;
+            }
+
+        }
+        static int Stars(int input)
+        {
+            return MAX_STAR * input / MAX;
+        }
+
+        #endregion
+
+        #region Controller
+        private static void Execute()
+        {
+            while (true)
+            {
+                InitGUI();
+                bool sort = true;
+                int[] sales = GetStars(sort, SALE);
+                int[] years = GetStars(sort, YEAR);
+
+                for (int i = 0; i < sales.GetLength(0); i++)
+                {
+                    Print("Year: " + years[i] + ": ");
+                    PrintStars(Stars(sales[i]));
+                    TerminateLine();
+                }
+                Console.ReadKey();
+            }
+        }
+
+        #endregion
+
+        #region View
+        private static void InitGUI()
+        {
+            Console.Clear();
+            Console.WriteLine(
+                "----------------------------------------------------------------------------------------------------------------\r\n" +
+                "                                        Danmarks Statestik for vin salg:\r\n" +
+                "----------------------------------------------------------------------------------------------------------------\r\n");
+        }
+        private static void TerminateLine()
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void Print(object input)
+        {
+            Console.Write(input);
+        }
+        static void PrintStars(int stars)
+        {
+            for (int i = 0; i < stars; i++)
+            {
+                if (i < 25)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else if (i < 50)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else if (i < 75)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                Print("*");
+            }
+        }
+
+        #endregion
+    }
+}
